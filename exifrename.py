e6a0af9d07fc4c5572821dc9cc1edebebdaca50e
Mike Maraya <mike.maraya@gmail.com>
2014-05-27 21:38:45 -0400

    read EXIF tags and display date image was taken

--- exifrename.py
+++ exifrename.py
@@ -4,15 +4,20 @@
 
 __author__ = 'Mike Maraya'
 __copyright__ = 'Copyright (c) 2014, Mike Maraya'
-__license__ = 'The MIT License (MIT)'
+__license__ = 'MIT'
 __version__ = '1.0'
 __email__ = 'mike.maraya@gmail.com'
 __status__ = 'Development'
 __maintainer__ = 'Mike Maraya'
 
-import glob, sys
+import exifread, glob, sys
 
 for pattern in (sys.argv[1:]):
     for filename in glob.glob(pattern):
         print 'File:', filename
+        f = open(filename, 'rb')
+        tags = exifread.process_file(f, details=False, stop_tag='EXIF DateTimeOriginal')
+        date = tags['EXIF DateTimeOriginal']
+        print 'Date %d', date
+
 
