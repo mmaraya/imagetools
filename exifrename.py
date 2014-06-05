861ed26231d834faf07d125eb48f1637bbb7c145
Mike Maraya <mike.maraya@gmail.com>
2014-06-04 20:30:54 -0400

    added comments, updated description

--- exifrename.py
+++ exifrename.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-"""exifrename.py: Rename a JPEG to the date and time the image was taken in YYYYMMDDHHMMSSxx.jpg format"""
+"""exifrename.py: Rename a JPEG to the date and time the image was taken in YYYYMMDDHHMMSSmss.jpg format"""
 
 __author__ = 'Mike Maraya'
 __copyright__ = 'Copyright (c) 2014, Mike Maraya'
@@ -26,7 +26,7 @@ for pattern in (sys.argv[1:]):
         file_date = time.localtime(os.path.getmtime(filename))
         if tags.has_key('EXIF DateTimeOriginal'):
             exif_date = tags['EXIF DateTimeOriginal']
-            file_date = time.strptime(str(exif_date), "%Y:%m:%d %H:%M:%S")
+            file_date = time.strptime(str(exif_date), '%Y:%m:%d %H:%M:%S')
         # get EXIF sub-second time or use 000
         counter = 0
         if tags.has_key('EXIF SubSecTime'):
@@ -34,6 +34,7 @@ for pattern in (sys.argv[1:]):
         # rename file
         dir = os.path.dirname(os.path.abspath(filename)) + os.sep
         new_filename = dir + time.strftime('%Y%m%d%H%M%S', file_date) + str(counter).zfill(3) + '.jpg'
+        # increase millisecond counter if filename already exists
         while os.path.isfile(new_filename):
             counter += 1
             new_filename = dir + time.strftime('%Y%m%d%H%M%S', file_date) + str(counter).zfill(3) + '.jpg'
