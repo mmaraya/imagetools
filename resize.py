9715de93abbd32d7f95be0c9188dbe767dd2e411
Mike Maraya <mike.maraya@gmail.com>
2014-06-10 22:50:41 -0400

    start new utility, resize.py

---
+++ resize.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""resize.py: Resize a JPEG to to the lowest resolution suitable for 8"x10" printing, preserving all EXIF information."""
+
+__author__ = 'Mike Maraya'
+__copyright__ = 'Copyright (c) 2014, Mike Maraya'
+__license__ = 'MIT'
+__version__ = '1.0'
+__email__ = 'mike.maraya@gmail.com'
+__status__ = 'Development'
+__maintainer__ = 'Mike Maraya'
