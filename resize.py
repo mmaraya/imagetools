d8307be8d33464ad0382e026ac0422bd6be9b58f
Mike Maraya <mike.maraya@gmail.com>
2014-06-11 23:38:41 -0400

    read image dimensions

--- resize.py
+++ resize.py
@@ -9,3 +9,17 @@ __version__ = '1.0'
 __email__ = 'mike.maraya@gmail.com'
 __status__ = 'Development'
 __maintainer__ = 'Mike Maraya'
+
+import glob
+import sys
+
+from PIL import Image
+
+
+for pattern in (sys.argv[1:]):
+    for filename in glob.glob(pattern):
+        try:
+            im = Image.open(filename)
+            print filename, im.format, "%dx%d" % im.size, im.mode
+        except IOError:
+            print 'Could not read image properties for %s' % (filename)
